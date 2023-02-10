f76ecc336dcdbc32872e6050ba639cbf01265c59
rapeerun <95340197+rapeerun@users.noreply.github.com>
2023-02-10 20:07:01 +0400

    Add files via upload

---
+++ w_4d_1exps/index.py
@@ -0,0 +1,89 @@
+
+# ex1 
+count=0
+while count < 4:
+    print("hello world")
+    count +=1
+
+# ex2 
+a_1=99
+b_1=3
+c_1=8
+print((a_1**b_1)*c_1)
+
+# ex3
+5 < 3    
+# False
+3 == 3
+# True
+3 == "3"
+# True
+# "3" > 3
+# False
+"Hello" == "hello"
+# True
+#  ex4
+computer_brand= "ASUS"
+
+print("I have a computer " + computer_brand )
+
+# ex5
+
+name="Raees"
+age=25
+shoe_size=float(12.5)
+info= "Hello my name is" + name,"" "I am"+str(age), "year old" "and my american is"+str(shoe_size)
+print(info)
+
+# ex6
+
+var_a=20    
+var_b=10
+if var_a >var_b:
+    print("Hello world")
+
+# ex7
+
+    num = int(input("Enter a number") )
+
+if (num % 2) == 0:
+
+              print("even")
+
+else:
+
+              print("odd")
+
+
+# ex8
+user_1=str("Raees")
+
+user=input("Enter your name")
+
+if user==user_1:
+    print("Welcome on board")
+else: print("go away")
+
+# ex9
+
+MIN_HEIGHT = 165 # Minimum height in cm
+
+height = int(input("Please enter your height in inches: "))
+height_cm = height * 2.54 # Convert inches to cm
+
+if height_cm >= MIN_HEIGHT:
+    print("You are tall enough to ride.")
+else:
+    print("You need to grow some more to ride.")
+
+
+
+
+
+
+
+
+
+
+
+
